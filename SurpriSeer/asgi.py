40271ae6a7f4ec5e6503a5a7502138fa686886f4
kiya2400 <kiya2400@student.miun.se>
2024-12-01 01:21:10 +0100

    Google auth

--- SurpriSeer/asgi.py
+++ SurpriSeer/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SurpriSeer project.
+ASGI config for surpriseer project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SurpriSeer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surpriseer.settings')
 
 application = get_asgi_application()
--- SurpriSeer/wsgi.py
+++ SurpriSeer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SurpriSeer project.
+WSGI config for surpriseer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SurpriSeer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'surpriseer.settings')
 
 application = get_wsgi_application()
